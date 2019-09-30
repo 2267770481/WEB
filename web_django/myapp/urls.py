08f19b71d398d8107242e98238956bd4e26982b7
2267770481 <2267770481@qq.com>
2019-09-30 10:25:38 +0800

    django

---
+++ web_django/myapp/urls.py
@@ -0,0 +1,25 @@
+"""web_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('index/', views.index),
+    path('set_cookie/', views.set_cookie),
+    path('get_cookie/', views.get_cookie),
+    path('set_session/', views.set_session),
+    path('get_session/', views.get_session),
+]
---
+++ web_django/myapp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    file_list = []
+    f = open("E:/python/code/爬虫/qiubai.txt")
+    while True:
+        lines = f.readlines(100)
+        if not lines:
+            break
+        for line in lines:
+            file_list.append(line.strip("'\n'").strip('\''))
+            # file_list.append(line)
+
+    f.close()
+    # print(file_list)
+    file_dic = {"file": str(file_list)}
+    print(file_dic)
+    return render(request, './myapp/index.html', file_dic)
+
+def set_cookie(request):
+    '''设置cookie'''
+    res = HttpResponse("设置cookie")
+    res.set_cookie('hello', 'cookie', expires=60 * 60 * 24 * 7)
+    return res
+    
+def get_cookie(request):
+    '''获取cookie'''
+    get = request.COOKIES.get('hello')
+    if get:
+        return HttpResponse('hello: ' + get)
+    else:
+        return HttpResponse('cookie不存在')
+
+def set_session(request):
+    '''设置session'''
+    request.session['hi'] = 'session'
+    return HttpResponse('设置session')
+    
+def get_session(request):
+    get = request.session['hi']
+    if get:
+        return HttpResponse('hi: ' + get)
+    else:
+        return HttpResponse('session不存在')
